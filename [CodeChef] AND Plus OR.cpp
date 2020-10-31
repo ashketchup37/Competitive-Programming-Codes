9e241c84334b9f67142e2e187e0f9286d51a27ce
Syed Ashir Abrar <fahi35@yahoo.com>
2020-10-31 23:12:17 +0600

    CodeChef Easy Math Problem.

---
+++ [CodeChef] AND Plus OR.cpp
@@ -0,0 +1,29 @@
+//https://www.codechef.com/LTIME89B/problems/ANDOR
+
+//Date: 31 Oct 2020
+//First Problem
+//October Lunchtime 2020 Division 2
+//Written By: daffy_duck
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+    freopen("inputfile.txt", "r", stdin);
+    freopen("outputfile.txt", "w", stdout);
+    #endif
+
+    long long t;
+    cin >> t;
+    while(t--)
+    {
+		//Or of a number, n and ZERO is always n
+    	char s[30];
+    	scanf("%s", s);
+    	printf("0 %s\n", s);
+    }
+}
+
